ed9ef81923157e3a58594fea3a4d41656ed90b24
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-29 02:27:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
-  array = [1,2,3]
 new_array = [1,4,9]
  
 array.length.times do |index|
-  new_array.push(array[index] + 0)
+  new_array.push(array[index] + 1)
 end
 new_array
 end
