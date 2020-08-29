902320570de029d80235bd48ad586e928c6c7dc0
Nicole Uyigue-Sivek <nicoleusivek@gmail.com>
2020-08-29 03:14:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
 new_array = []
-
+counter = 0
 while array[counter] do
-new_array.push(array[index]).square
+new_array.push(array[counter].square)
+counter += 1
 end
 new_array
 end
